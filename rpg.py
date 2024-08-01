c0ba527a8c95a412aed89c91b97c45d5f233bd00
AqibAamir <144609363+AqibAamir@users.noreply.github.com>
2024-08-01 16:40:25 +0100

    Update rpg.py

--- rpg.py
+++ rpg.py
@@ -55,3 +55,66 @@ class Character:
 
     def is_alive(self):
         return self.health > 0
+
+
+   def attack_enemy(self, enemy):
+        damage = self.attack - enemy.defense
+        if damage < 0:
+            damage = 0
+        enemy.take_damage(damage)
+        return damage
+
+# Define the Player class
+class Player(Character):
+    def __init__(self, name):
+        super().__init__(name, 100, 10, 5)
+        self.level = 1
+        self.experience = 0
+        self.experience_to_next_level = 100
+        self.gold = 0
+        self.inventory = []
+        self.equipped_weapon = None
+        self.equipped_armor = None
+        self.active_quests = []
+        self.special_abilities = {
+            "Fireball": {"damage": 20, "cost": 10},
+            "Heal": {"heal_amount": 30, "cost": 5}
+        }
+
+    def gain_experience(self, amount):
+        self.experience += amount
+        if self.experience >= self.experience_to_next_level:
+            self.level_up()
+
+    def level_up(self):
+        self.level += 1
+        self.experience = 0
+        self.experience_to_next_level *= 2
+        self.health += 10
+        self.attack += 2
+        self.defense += 1
+        print(f"{self.name} leveled up to level {self.level}!")
+
+    def add_gold(self, amount):
+        self.gold += amount
+
+    def add_item(self, item):
+        self.inventory.append(item)
+
+    def equip_weapon(self, weapon):
+        if self.equipped_weapon:
+            self.attack -= self.equipped_weapon.attack_boost
+        self.equipped_weapon = weapon
+        self.attack += weapon.attack_boost
+
+    def equip_armor(self, armor):
+        if self.equipped_armor:
+            self.defense -= self.equipped_armor.defense_boost
+        self.equipped_armor = armor
+        self.defense += armor.defense_boost
+
+    def use_consumable(self, consumable):
+        self.health += consumable.heal_amount
+        if self.health > 100:
+            self.health = 100
+
